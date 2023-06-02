32a697cad86e17588fb61da9275c0ed614080917
Yohana Avila <yohana.avila3009@gmail.com>
2023-06-02 00:28:37 -0500

    Agregando propiedades al fotter para que siempre esté abajo

--- Administradores/clientes.php
+++ Administradores/clientes.php
@@ -86,5 +86,11 @@
             </div>
         </div>
 
+        <!-- Footer-->
+        <footer class="py-5 bg-dark" style="position: fixed;bottom: 0; width: 100%; ">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Bryan Sánchez & Yohana Avila</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
